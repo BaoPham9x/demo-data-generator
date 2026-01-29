90acc57fbf9dc99a50d2b1014a09e5d47d0aff36
Bao Ngoc Pham <ngocphamftu@gmail.com>
2026-01-29 21:14:22 +0100

    fix warning and add target

--- star-schema/models/marts/fact_balances.sql
+++ star-schema/models/marts/fact_balances.sql
@@ -11,10 +11,12 @@ WITH all_balances AS (
 SELECT
     b.balance_snapshot_id,
     da.account_key,
+    dc.customer_key,
     b.balance_amount,
     b.currency,
     b.balance_date,
     b.created_at
 FROM all_balances b
 LEFT JOIN {{ ref('dim_account') }} da ON b.account_id = da.account_id
+LEFT JOIN {{ ref('dim_customer') }} dc ON da.customer_id = dc.customer_id
 WHERE b.balance_date <= CURRENT_DATE()
